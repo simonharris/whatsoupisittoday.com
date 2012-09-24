1893c9f6a7dd7bc0feb599aad63cbb4ccf998cb2
Simon Harris <github@pointbeing.net>
2012-09-24 21:09:55 +0100

    Update Tossed soup for new non-rotating menu

--- _scripts/fetch_tossed.py
+++ _scripts/fetch_tossed.py
@@ -11,14 +11,23 @@ outfile = '/tmp/tossed.pkl';
 
 
 # update 20120612
-souplist = [
-        ['Chickpea & Spinach Dhal', 'Malaysian Chicken', 'Tomato & Basil'],
-        ['Sweet Potato, Coconut & Coriander ', 'Summer Chicken', 'Tomato & Basil'],
-        ['Pea & Mint', 'Chilli Chicken', 'Tomato & Basil'],
-        ['Minestrone', 'Indonesian Chicken Noodle', 'Tomato & Basil'],
-        ['Pick of the Week', 'Tomato & Basil'],
-]
+#souplist = [
+#        ['Chickpea & Spinach Dhal', 'Malaysian Chicken', 'Tomato & Basil'],
+#        ['Sweet Potato, Coconut & Coriander ', 'Summer Chicken', 'Tomato & Basil'],
+#        ['Pea & Mint', 'Chilli Chicken', 'Tomato & Basil'],
+#        ['Minestrone', 'Indonesian Chicken Noodle', 'Tomato & Basil'],
+#        ['Pick of the Week', 'Tomato & Basil'],
+#]
 
+
+# update 20120924
+souplist = [
+        ['Malaysian Chicken', 'Tomato & Basil'],
+        ['Malaysian Chicken', 'Tomato & Basil'],
+        ['Malaysian Chicken', 'Tomato & Basil'],
+        ['Malaysian Chicken', 'Tomato & Basil'],
+        ['Malaysian Chicken', 'Tomato & Basil'],
+]
 
 output = open(outfile, 'wb')
 pickle.dump(souplist, output, -1)
