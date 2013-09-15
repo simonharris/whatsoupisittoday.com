d7d5c622c158dd0ba19ca525515daa6f5ee583ef
Simon Harris <github@pointbeing.net>
2013-09-15 21:37:53 +0100

    Update Tossed soup list for Autumn

--- _scripts/fetch_tossed.py
+++ _scripts/fetch_tossed.py
@@ -4,38 +4,18 @@ import pickle
 """
 Store Tossed soup data in a list and pickle it
 
-Run: by hand, as and when
+Run: by hand, when updates supplied by Tossed
 """
 
 outfile = '/tmp/tossed.pkl';
 
-
-# update 20120612
-#souplist = [
-#        ['Chickpea & Spinach Dhal', 'Malaysian Chicken', 'Tomato & Basil'],
-#        ['Sweet Potato, Coconut & Coriander ', 'Summer Chicken', 'Tomato & Basil'],
-#        ['Pea & Mint', 'Chilli Chicken', 'Tomato & Basil'],
-#        ['Minestrone', 'Indonesian Chicken Noodle', 'Tomato & Basil'],
-#        ['Pick of the Week', 'Tomato & Basil'],
-#]
-
-
-# update 20120924
-#souplist = [
-#        ['Malaysian Chicken', 'Tomato & Basil', 'Classic Chicken', 'Lentil Dhal'],
-#        ['Malaysian Chicken', 'Tomato & Basil', 'Classic Chicken', 'Lentil Dhal'],
-#        ['Malaysian Chicken', 'Tomato & Basil', 'Classic Chicken', 'Lentil Dhal'],
-#        ['Malaysian Chicken', 'Tomato & Basil', 'Classic Chicken', 'Lentil Dhal'],
-#        ['Malaysian Chicken', 'Tomato & Basil', 'Classic Chicken', 'Lentil Dhal'],
-#]
-
-# update 20130401
+# update 20130915
 souplist = [
-	['100 Calorie Tomato and Basil','100 Calorie Malaysian Chicken','150 Calorie Pea and Mint','150 Calorie Summer Chicken','Super Skinny Miso'],
-	['100 Calorie Tomato and Basil','100 Calorie Malaysian Chicken','150 Calorie Pea and Mint','150 Calorie Summer Chicken','Super Skinny Miso'],
-	['100 Calorie Tomato and Basil','100 Calorie Malaysian Chicken','150 Calorie Pea and Mint','150 Calorie Summer Chicken','Super Skinny Miso'],
-	['100 Calorie Tomato and Basil','100 Calorie Malaysian Chicken','150 Calorie Pea and Mint','150 Calorie Summer Chicken','Super Skinny Miso'],
-	['100 Calorie Tomato and Basil','100 Calorie Malaysian Chicken','150 Calorie Pea and Mint','150 Calorie Summer Chicken','Super Skinny Miso']
+	['Low GI Minestrone', 'Big Protein Chicken', '100 Calorie Tomato and Basil','100 Calorie Malaysian Chicken'],
+	['Low GI Minestrone', 'Big Protein Chicken', '100 Calorie Tomato and Basil','100 Calorie Malaysian Chicken'],
+	['Low GI Minestrone', 'Big Protein Chicken', '100 Calorie Tomato and Basil','100 Calorie Malaysian Chicken'],
+	['Low GI Minestrone', 'Big Protein Chicken', '100 Calorie Tomato and Basil','100 Calorie Malaysian Chicken'],
+	['Low GI Minestrone', 'Big Protein Chicken', '100 Calorie Tomato and Basil','100 Calorie Malaysian Chicken']
 ]
 
 output = open(outfile, 'wb')
