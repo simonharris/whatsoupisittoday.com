30d834d36323fc86b8c488302d71f75b3bccee51
Simon Harris <github@pointbeing.net>
2013-09-16 00:27:32 +0100

    Update Leon fetcher since they now have soup again

--- _scripts/fetch_leon.py
+++ _scripts/fetch_leon.py
@@ -1,17 +1,31 @@
 from lxml.html import parse
 from pprint import pprint
 import pickle
-import simplejson
-import urllib 
-
-
-
+import re
 
 """
-Fetch today's soup from Leon's website
+Fetch this week's soup from Leon's website
 
-Run: daily, early each morning
+Run: daily, early morning
 """
 
+outfile = '/tmp/leon.pkl';
+soupurl = 'http://www.leonrestaurants.co.uk/';
 
-# todo
+def fix_text(astr) :
+	#astr = astr.strip()
+	#astr = astr.replace('has nuts in it', '')
+	#astr = re.sub('/NEW /', '', astr)
+	#astr = re.sub('/ - Back by popular demand!$/', '', astr);
+	#astr = astr.strip()
+	return astr
+
+doc = parse(soupurl)
+elements = doc.xpath('//div[@class="soup"]//h2')
+
+roughlist = [elem.text_content() for elem in elements]
+souplist = map(fix_text, roughlist)
+
+output = open(outfile, 'wb')
+pickle.dump(souplist, output, -1)
+output.close()
