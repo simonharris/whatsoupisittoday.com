9e130fe3b66ebe8b2791d4120741c18358602c2d
Simon Harris <github@pointbeing.net>
2013-10-13 17:08:56 +0100

    Add fetcher and styles for Pure

---
+++ _scripts/fetch_pure.py
@@ -0,0 +1,46 @@
+from pprint import pprint
+import pickle
+from datetime import date
+
+"""
+Store Pure soup data in a list and pickle it
+
+Run: weekly
+"""
+
+outfile = '/tmp/pure.pkl';
+
+#
+# Pure have a two-week rotation, so just define the whole menu here and decide
+# which week it currently is later on.
+#
+souplist = {
+	'even':[
+		['Spiced Carrot', 'Tomato & Charred Red Onion', 'Siam Chicken'],
+		['Leek & Potato', 'Thai green lentil, Spinach & Coconut', 'Chicken Sambar Dahl'],
+		['Winter Chicken Noodle', 'Portobello Mushroom with Thyme', 'Lakeland Beef with Potato'],
+		['Tomato & Basil', 'Moroccan Sweet Potato & Chickpea', 'Bean & Bacon Broth'],
+		['Winter Vegetable & Barley', 'Tomato & Sweet Red Pepper', 'Tuscan Sausage Broth']
+	],
+	'odd':[
+		['Tomato & Basil', 'Moroccan Sweet Potato & Chickpea', 'Lamb & Barley'],
+		['Spiced Carrot', 'Tomato & Sweet Red Pepper', 'Chicken Laksa'],
+		['Leek & Potato', 'Portobello Mushroom with Thyme', 'Chipotle Beef'],
+		['Winter Chicken Noodle	', 'Tomato & Charred Red Onion', 'Boston Chicken Chowder'],
+		['Winter Vegetable & Barley', 'Thai Green Lentil, Spinach & Coconut', 'Chorizo Puchero']
+	]
+}
+
+# We'll run this over the weekend so we actually want next week
+weeknum = date.today().isocalendar()[1] + 1
+
+if (weeknum % 2) :
+	whichweek = 'odd'
+else :
+	whichweek = 'even'
+
+thisweek = souplist[whichweek]
+
+output = open(outfile, 'wb')
+pickle.dump(thisweek, output, -1)
+output.close()
--- soup/urls.py
+++ soup/urls.py
@@ -1,30 +1,26 @@
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    #(r'^about/?', 'about.views.index'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
-
+    # home page
     url(r'^$', 'listings.views.show_soup', {'which':'pret', 'tomorrow':False}, name='n_home'),
-    url(r'^pret/?$', 'listings.views.show_soup', {'which':'pret', 'tomorrow':False}, name='n_pret_today'),
-    url(r'^pret/tomorrow/?$', 'listings.views.show_soup', {'which':'pret', 'tomorrow':True}, name='n_pret_tomorrow'),
-    url(r'^eat/?$', 'listings.views.show_soup', {'which':'eat', 'tomorrow':False}, name='n_eat_today'),
-    url(r'^eat/tomorrow/?$', 'listings.views.show_soup', {'which':'eat', 'tomorrow':True}, name='n_eat_tomorrow'),
-    url(r'^tossed/?$', 'listings.views.show_soup', {'which':'tossed', 'tomorrow':False}, name='n_tossed_today'),
-    url(r'^tossed/tomorrow/?$', 'listings.views.show_soup', {'which':'tossed', 'tomorrow':True}, name='n_tossed_tomorrow'),
+
+    # soup pages
     url(r'^abokado/?$', 'listings.views.show_soup', {'which':'abokado', 'tomorrow':False}, name='n_abokado_today'),
     url(r'^abokado/tomorrow/?$', 'listings.views.show_soup', {'which':'abokado', 'tomorrow':True}, name='n_abokado_tomorrow'),
+    url(r'^eat/?$', 'listings.views.show_soup', {'which':'eat', 'tomorrow':False}, name='n_eat_today'),
+    url(r'^eat/tomorrow/?$', 'listings.views.show_soup', {'which':'eat', 'tomorrow':True}, name='n_eat_tomorrow'),
     url(r'^leon/?$', 'listings.views.show_soup_single', {'which':'leon'}, name='n_leon_today'),
     url(r'^nusa/?$', 'listings.views.show_soup_single', {'which':'nusa'}, name='n_nusa_today'),
+    url(r'^pret/?$', 'listings.views.show_soup', {'which':'pret', 'tomorrow':False}, name='n_pret_today'),
+    url(r'^pret/tomorrow/?$', 'listings.views.show_soup', {'which':'pret', 'tomorrow':True}, name='n_pret_tomorrow'),
+    url(r'^pure/?$', 'listings.views.show_soup', {'which':'pure', 'tomorrow':False}, name='n_pure_today'),
+    url(r'^pure/tomorrow/?$', 'listings.views.show_soup', {'which':'pure', 'tomorrow':True}, name='n_pure_tomorrow'),
+    url(r'^tossed/?$', 'listings.views.show_soup', {'which':'tossed', 'tomorrow':False}, name='n_tossed_today'),
+    url(r'^tossed/tomorrow/?$', 'listings.views.show_soup', {'which':'tossed', 'tomorrow':True}, name='n_tossed_tomorrow'),
+
+    # about page
     url(r'^about/?$', 'pages.views.about', name='n_pages_about'),
 )
 
