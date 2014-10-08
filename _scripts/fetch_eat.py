11747c0b85ee0a33e0f2cbb6fc5674228da28fc7
Simon Harris <pointbeing@github.com>
2014-10-08 17:01:50 +0000

    Fix Eat fetcher to work with new page design

--- _scripts/fetch_eat.py
+++ _scripts/fetch_eat.py
@@ -2,6 +2,7 @@ from lxml.html import parse
 from pprint import pprint
 import pickle
 import re
+import datetime
 
 """
 Fetch this week's soup from EAT's website
@@ -10,30 +11,47 @@ Run: weekly, early Monday morning
 """
 
 outfile = '/tmp/eat.pkl';
-soupurl = 'http://www.eatserver.co.uk/editor/menuProvider.php';
+soupurl = 'http://eat.co.uk/';
 
 def fix_text(astr) :
-	astr = astr.strip()
-	astr = astr.replace('has nuts in it', '')
-	astr = astr.replace('(less than 5% fat)', '')
-	astr = re.sub('/NEW /', '', astr)
-	astr = re.sub('/ - Back by popular demand!$/', '', astr);
+	#astr = astr.strip()
+	#astr = astr.replace('has nuts in it', '')
+	#astr = astr.replace('(less than 5% fat)', '')
+	#astr = re.sub('/NEW /', '', astr)
+	astr = astr.replace('\n', ' ')
+	#astr = re.sub('/\t/', '', astr)
+	#astr = re.sub('/ - Back by popular demand!$/', '', astr);
 	astr = astr.strip()
 	return astr
 
 doc = parse(soupurl)
-elements = doc.xpath('//strong')
+elements = doc.xpath('//h4')
 
 roughlist = [elem.text_content() for elem in elements]
 roughlist = map(fix_text, roughlist)
 
+#pprint(roughlist)
+
+#fragile
+today1    = roughlist[3]
+today2    = roughlist[4]
+tomorrow1 = roughlist[5]
+tomorrow2 = roughlist[6]
+
+today = datetime.datetime.today().weekday()
+
 souplist = [
-	[roughlist[6], roughlist[4]],   # mon
-	[roughlist[10], roughlist[8]],  # tues
-	[roughlist[14], roughlist[12]], # weds
-	[roughlist[18], roughlist[16]], # thurs
-	[roughlist[22], roughlist[20]], # fri
+	[], # mon
+	[], # tues
+	[], # weds
+	[], # thurs
+	[], # fri
 ]
+
+souplist[today]   = [today1, today2]
+souplist[today+1] = [tomorrow1, tomorrow2]
+
+#pprint(souplist)
 
 output = open(outfile, 'wb')
 pickle.dump(souplist, output, -1)
